0d4ec69e3a985c9a824262859d7fe14847f4c731
kore01 <58136304+kore01@users.noreply.github.com>
2020-02-27 23:33:44 +0200

    Accounts

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project_Vacation_Manager.Data
+{
+    public class ApplicationDbContext : IdentityDbContext <ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
